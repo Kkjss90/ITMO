ee6887c0c44c25671174c7c03626ca26066f9730
kkjss90 <sasha170804@gmail.com>
2023-01-05 18:16:22 +0300

    done

---
+++ prog_labs/lab_3/utility/Human.java
@@ -0,0 +1,45 @@
+package utility;
+
+import characters.Neznayka;
+import objects.Spacesuit;
+
+import java.util.Objects;
+
+public abstract class Human implements Movable {
+    private final String name;
+
+    public Human(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+    public abstract void walkBy(Object obj);
+    public abstract void stopWalking(Object obj);
+    public abstract void jump();
+    public abstract void carry(Object obj);
+    public abstract void dressOff(Spacesuit spacesuit, Neznayka neznayka);
+    @Override
+    public boolean equals(Object obj){
+        if (this == obj) return true;
+        if (obj == null) return false;
+        if (getClass() != obj.getClass()) return false;
+        Human human = (Human) obj;
+
+        return name.equals(human.name);
+    }
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(name);
+    }
+    @Override
+    public void move(){
+        System.out.println("Подвинут");
+    }
+    @Override
+    public String toString() {
+        return "Персонаж, которого зовут '" + name + "'";
+    }
+}
---
+++ prog_labs/lab_3/utility/Location.java
@@ -0,0 +1,6 @@
+package utility;
+
+public enum Location {
+    OUTSIDE,
+    INSIDE
+}
