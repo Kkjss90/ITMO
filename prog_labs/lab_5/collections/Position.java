2c712bbb58266115c055b245834249c4c7564ed8
Kkjss90 <sasha170804@gmail.com>
2023-02-11 11:11:37 +0300

    ?

---
+++ prog_labs/lab_5/collections/Position.java
@@ -0,0 +1,7 @@
+package collections;
+
+public class Position {
+    private Float x; //Поле не может быть null
+    private Float y; //Поле не может быть null
+    private Double z; //Поле не может быть null
+}
