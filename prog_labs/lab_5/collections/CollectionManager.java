e8f682665d55543ecb6380f561bf92938c0cf0e2
Kkjss90 <sasha170804@gmail.com>
2023-02-18 19:12:42 +0300

    to be continued...

---
+++ prog_labs/lab_5/collections/CollectionManager.java
@@ -0,0 +1,11 @@
+package collections;
+
+import file.*;
+import java.util.Vector;
+
+/**
+ * Менеджер коллекции - класс отвечающий за работу с коллекциями
+ */
+public class CollectionManager {
+
+}
