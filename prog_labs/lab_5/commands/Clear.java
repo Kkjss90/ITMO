1ff33e0d62a5a32613b24b837e0f17cf800a1946
kkjss90 <sasha170804@gmail.com>
2023-03-22 20:05:29 +0300

    done

--- prog_labs/lab_5/commands/Clear.java
+++ prog_labs/lab_5/commands/Clear.java
@@ -1,10 +1,27 @@
 package commands;
 
 public class Clear implements Command {
+    /**
+     * Поле, хранящее ссылку на объект класса CollectionManager.
+     */
+    private CollectionManager collectionManager;
 
+    /**
+     * Конструктор класса
+     *
+     * @param collectionManager Хранит ссылку на созданный в объекте Application объект CollectionManager.
+     */
+    public ClearCommand(CollectionManager collectionManager) {
+        this.collectionManager = collectionManager;
+    }
+
+    /**
+     * Метод, исполняющий команду. Выводит сообщение когда коллекция очищена.
+     */
     @Override
     public void execute() {
-
+        collectionManager.clear();
+        System.out.println("Коллекция была очищена");
     }
 
     @Override
