8346ec962f9ee50c11ee67923054e594f669afa5
kkjss90 <sasha170804@gmail.com>
2023-03-23 17:00:38 +0300

    done

--- prog_labs/lab_5/file/RouteFieldsReader.java
+++ prog_labs/lab_5/file/RouteFieldsReader.java
@@ -18,6 +18,7 @@ public class RouteFieldsReader {
     public RouteFieldsReader(UserIO userIO) {
         this.userIO = userIO;
     }
+
     /**
      * Метод, выводящий производящий чтение данных из консоли. Запрашивает ввод полей в строго определенном порядке.
      *
@@ -28,6 +29,7 @@ public class RouteFieldsReader {
         String i = Instant.now().toString();
         return new Route(id, readName(), readCoordinates(), readFrom(), readTo(), readDistance(), LocalDate.parse(i));
     }
+
     /**
      * Метод, производящий чтение поля name типа String объекта Route из потока, указанного в поле userIO. При некорректном вводе просит ввести поля заново.
      *
@@ -43,6 +45,22 @@ public class RouteFieldsReader {
             else return str;
         }
     }
+
+    public float readDistance() {
+        float distance;
+        while (true) {
+            userIO.printCommandText("distance (not null & > 1): ");
+            distance = float.parseFloat(userIO.readLine().trim());
+            if (distance <= 1) throw new ValidValuesRangeException();
+            else return distance;
+        } catch (ValidValuesRangeException ex) {
+            System.out.println("distance должна быть больше 1");
+        } catch (NumberFormatException ex) {
+            System.err.println("Число должно быть типа float и не null");
+        }
+        }
+    }
+
     /**
      * Метод, производящий чтение координат x, y.
      *
@@ -63,12 +81,12 @@ public class RouteFieldsReader {
             try {
                 userIO.printCommandText("coordinate_x (float & not null & x > -443): ");
                 x = Float.parseFloat(userIO.readLine().trim());
-                if (x <= -648) throw new ValidValuesRangeException();
+                if (x <= -443) throw new ValidValuesRangeException();
                 else return x;
             } catch (ValidValuesRangeException ex) {
                 System.out.println("Координата x должна быть больше -443");
             } catch (NumberFormatException ex) {
-                System.err.println("Число должно быть типа Double и не null");
+                System.err.println("Число должно быть типа Float и не null");
             }
         }
     }
@@ -83,35 +101,37 @@ public class RouteFieldsReader {
         while (true) {
             try {
                 userIO.printCommandText("coordinate_y (int & can be null): ");
-                String str = userIO.readLine().trim();
-                if (str.equals("")) y = 0;
+                y = int.parseInt(userIO.readLine().trim());
                 return y;
             } catch (NumberFormatException ex) {
                 System.err.println("Число должно быть типа int");
             }
         }
     }
-    public double readCoordinate_X(){
+    public double readCoordinate_X() {
         double x;
-        while (true){
-            try{
+        while (true) {
+            try {
                 userIO.printCammandText("coordinate_x (double & can be null): ");
-                String str = userIO.readLine().trim();
-                if (str.equals("")) x = 0;
-            }catch (NemberFormatException ex){
+                x = double.parseDouble(userIO.readLine().trim());
+                if (x==0) throw new ValidValuesRangeException();
+                else return x;
+            } catch (ValidValuesRangeException ex) {
+                System.err.println("Число должно быть типа Double и не null");
+            } catch (NumberFormatException ex) {
                 System.err.println("Число должно быть типа double");
             }
         }
     }
-    public int readCoordinate_Y(){
-        int y;
+    public Float readCoordinate_Y(){
+        Float y;
         while (true){
             try{
-                userIO.printCammandText("coordinate_y (int & can be null): ");
-                String str = userIO.readLine().trim();
-                if (str.equals("")) y = 0;
-            }catch (NemberFormatException ex){
-                System.err.println("Число должно быть типа int");
+                userIO.printCammandText("coordinate_y (float & not null): ");
+                y = Float.parseFloat(userIO.readLine().trim());
+                return y;
+            }catch (NumberFormatException ex){
+                System.err.println("Число должно быть типа float");
             }
         }
     }
@@ -120,18 +140,35 @@ public class RouteFieldsReader {
         while (true){
             try{
                 userIO.printCammandText("coordinate_z (float & not null): ");
-                String str = userIO.readLine().trim();
-                if (z = null) throw new ValidValuesRangeException();
+                z = Float.parseFloat(userIO.readLine().trim());
+                if (z == 0) throw new ValidValuesRangeException();
                 else return z;
-            }catch (NemberFormatException ex){
+            }catch (NumberFormatException ex){
                 System.err.println("Число должно быть типа float и не null");
+            }catch (ValidValuesRangeException ex){
+                System.err.println("Число должно быть не null");
             }
         }
     }
     public Location readFrom(){
-        return new Location(readName(), readCoordinate_X(), readCoordinate_Y(), readCoordinate_Z());
+        return new Location(readName(), readCoordinate_X(), readCoordinateY(), readCoordinate_Z());
+    }
+    public double readCoordinateZ() {
+        double z;
+        while (true) {
+            try{
+                userIO.printCammandText("coordinate_z (double & not null): ");
+                z = double.parseDouble(userIO.readLine().trim());
+                if (z==0) throw new ValidValuesRangeException();
+                else return z;
+            }catch (NumberFormatException ex){
+                System.err.println("Число должно быть типа float и не null");
+            }catch (ValidValuesRangeException ex) {
+                System.err.println("Число должно быть не null");
+            }
+        }
     }
     public Position readTo(){
-        return
+        return new Position(readCoordinateX(), readCoordinate_Y(), readCoordinateZ());
     }
 }
