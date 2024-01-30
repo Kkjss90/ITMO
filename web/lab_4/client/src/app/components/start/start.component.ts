5a45c6a767e3d0588a413616f880ad047b2e1d6f
kkjss90 <sasha170804@gmail.com>
2024-01-31 00:44:54 +0300

    ok

--- web/lab_4/client/src/app/components/start/start.component.ts
+++ web/lab_4/client/src/app/components/start/start.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-start',
@@ -7,6 +7,27 @@ import { Component } from '@angular/core';
   templateUrl: './start.component.html',
   styleUrl: './start.component.css'
 })
-export class StartComponent {
+export class StartComponent implements OnInit {
+  ngOnInit() {
+    this.updateTime();
 
+    setInterval(() => {
+      this.updateTime();
+    }, 6000);
+  }
+
+  private updateTime() {
+    const seconds: number = new Date().getSeconds();
+
+    //Seconds
+    document.getElementById("seconds")!.innerHTML = (seconds < 10 ? '0' : '') + seconds;
+
+    // Minutes
+    const minutes: number = new Date().getMinutes();
+    document.getElementById("minutes")!.innerHTML = (minutes < 10 ? '0' : '') + minutes;
+
+    // Hours
+    const hours: number = new Date().getHours();
+    document.getElementById("hours")!.innerHTML = (hours < 10 ? '0' : '') + hours;
+  }
 }
